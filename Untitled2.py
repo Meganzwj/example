f29d453d093b27aa34601f0c7932016122bd8f6c
Meganzwj <87553729+Meganzwj@users.noreply.github.com>
2022-08-29 17:41:16 -0400

    Add files via upload

---
+++ Untitled2.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import spacy
+nlp = spacy.load("en_core_web_sm")
+
+
+# In[3]:
+
+
+with open ("C:/Users/zhuwe/Desktop/CV/learning materials/SQL/CRA/Azure/groundtruth.txt","r") as f:
+    text=f.read()
+    print (text)
+
+
+# In[4]:
+
+
+with open ("C:/Users/zhuwe/Desktop/CV/learning materials/SQL/CRA/Azure/generated.txt","r") as g:
+    text1=g.read()
+    print (text1)
+
+
+# In[5]:
+
+
+doc =nlp(text)
+doc1 =nlp(text1)
+
+
+# In[6]:
+
+
+doc.similarity(doc1)
+
+
+# In[7]:
+
+
+doc1.similarity(doc)
+
+
+# In[ ]:
+
+
+
+
